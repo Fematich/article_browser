00a2217e2a256e58fff5ccfad8fa5450fba77c9f
Matthias Feys <matthiasfeys@gmail.com>
2013-10-15 21:41:33 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'you-will-never-guess'
+MAX_SEARCH_RESULTS = None
+RES_PER_PAGE=50
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os, subprocess, sys
+#subprocess.call(['python', 'virtualenv.py', 'flask'])
+bin = 'bin'
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-mail'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy==0.7.9'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-sqlalchemy'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy-migrate'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-whooshalchemy'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-wtf'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-babel'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flup'])
